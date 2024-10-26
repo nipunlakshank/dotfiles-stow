5bff59f85a0f2166d1b0d14c7cd8026d3359c332
Nipun Lakshan <nipunlakshankumara@gmail.com>
2024-10-26 06:07:41 +0530

    Add a script to automate installation

--- setup.sh
+++ setup.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env bash
 
-print "\33[92m Step 1 --> Installing Homebrew... \33[0m"
+config=${1:-$(scutil --get LocalHostName)}
+
+printf "\33[92mStep 1 --> Installing Homebrew... \33[0m\n"
 if command -v brew &> /dev/null; then
     echo "Homebrew is already installed: $(brew --version)"
 else
     curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | /bin/bash
-    print "\33[92m Step 1 Complete...\n\n \33[0m"
 fi
 
 
-print "\33[92m Step 2 --> Installing nix... \33[0m"
-if command -v brew &> /dev/null; then
-    echo "Nix is already installed: $(nix --version)"
+printf "\n\33[92mStep 2 --> Installing nix... \33[0m\n"
+if [ "$(find /nix -mindepth 1 -print -quit 2> /dev/null)" ]; then
+    echo "Nix is already installed"
 else
     sh <(curl -L https://nixos.org/nix/install)
-    print "\33[92m Step 2 Complete...\n\n \33[0m"
 fi
 
-print "\33[92m Step 3 --> Installing nix-darwin... \33[0m"
+printf "\n\33[92mStep 3 --> Installing nix-darwin... \33[0m\n"
 if command -v darwin-rebuild &> /dev/null; then
-    echo "nix-darwin is already installed"
+    echo "nix-darwin is already installed. Rebuilding flake for configuration: ${config}..."
+    darwin-rebuild switch --flake ~/dotfiles/nix-darwin#${config}
 else
-    nix run nix-darwin -- switch --flake ~/dotfiles/nix-darwin#air
-    print "\33[92m Step 3 Complete...\n\n \33[0m"
+    echo "Building nix-darwin flake for configuration: ${config}..."
+    /nix/var/nix/profiles/default/bin/nix run nix-darwin -- switch --flake ~/dotfiles/nix-darwin#${config}
 fi
+
